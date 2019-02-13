409fd547ff6eb0dd20141b0e71c483095ec8bfa1
David <brante_krantz@hotmail.com>
2019-02-13 16:55:32 +0100

    Include avatar and names in contact API

--- app/serializers/api/contacts_serializer.rb
+++ app/serializers/api/contacts_serializer.rb
@@ -6,11 +6,7 @@ class Api::ContactsSerializer < ActiveModel::Serializer
   end
 
   class Api::ContactsSerializer::Index < ActiveModel::Serializer
-<<<<<<< HEAD
-    attributes(:email, :name, :text, :avatar)
-=======
     attributes(:id, :email, :name, :text, :avatar)
->>>>>>> 10c48023... Include avatar and names in contact API
     attribute(:users) { object.users.collect{|u| u.firstname + ' ' + u.lastname} }
 
     def text

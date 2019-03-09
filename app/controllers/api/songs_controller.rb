b5f6e261f67bfb14af8c251238119cdb0af77271
Sofie Hellmark <sofie.hellmark@gmail..com>
2019-03-09 11:37:21 +0100

    back to start

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -6,10 +6,10 @@ class Api::SongsController < Api::BaseController
     render json: @songs,
     each_serializer: Api::SongSerializer::Index # Returns id and title
   end
-# hej 
+
   def show
     @song = Song.find(params[:id])
-    Song.increment_counter(:vistits, @song)
+    Song.increment_counter(:visits, @song)
     render json: @song,
     serializer: Api::SongSerializer::Show # Returns all relevant fields
   end
